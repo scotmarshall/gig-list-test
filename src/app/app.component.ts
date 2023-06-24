a95f840b4d333623a6b200d9d82b86144022c02f
Scot Marshall <scotmarshall@hotmail.com>
2023-06-24 21:20:31 +1000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Gig List -- Test';
+  jsonData: any;
+  keys: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchJsonData();
+  }
+
+  convertDate(gigDate: any) {
+    var d = new Date(gigDate);
+    return d.toDateString() + ", " + d.toLocaleTimeString();
+  }
+
+  fetchJsonData() {
+    this.http.get('https://scotmarshall.github.io/Data/gigs.json').subscribe((data: any) => {
+      this.jsonData = data;
+      this.keys = Object.keys(this.jsonData.gigs);
+    });
+  }
+}
